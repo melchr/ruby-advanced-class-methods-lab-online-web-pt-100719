bd1fbcb9fd36cac4f3833189a31b733ebafebf7b
Melissa Christman <melchrstmn@gmail.com>
2019-10-30 23:03:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,26 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(title)
+    song = self.new
+    song.name = title
+    song
+  end
+  
+  def self.create_by_name(title)
+    song = self.new
+    song.name = title
+    @@all << song
+    song
+  end
+  
+  def self.find_by_name(title)
+  
 end
